b8bcd64ebc8eb67890fc4209bdf22e7c799bd821
Link-Secret <zj1078184113@163.com>
2018-04-25 11:05:17 +0800

    伯乐在线爬取

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
-#
+# 请求地址
 from scrapy.http import Request
+# 拼接URL地址的函数
 from urllib import parse
 
 class JobboleSpider(scrapy.Spider):
@@ -16,21 +17,31 @@ class JobboleSpider(scrapy.Spider):
                 2. 获取下一页URL并交给scrapy下载，下载完成后交给parse
         '''
 
-        '''解析列表页中的所有文章URL并交给scrapy下载后并进行解析'''
+        '''1-解析列表页中的所有文章URL并交给scrapy下载后并进行解析'''
         post_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)").extract()
+        # 对获取的当前页(所有文章页)的所有文章详情页地址遍历
         for post_url in post_urls:
             # 每个超链都调用parse_detail函数，得到具体的内容
-            yield Request(url= parse.urljoin(response.url,post_url),callback=self.parse_detail())
-            print(post_url)
+            # yield Request(url = post_url,callback=self.parse_detail)
 
+            '''伯乐网的超链是完整地址，只是为了演示有的网站超链基于当前地址（不是完整地址）所以使用这个URLjoin'''
+            # yield关键字会自动交给scrapy进行下载
+            # Request函数执行完url的爬取后都会调用(回调函数)self.parse_detail函数,即提取文章具体字段，循环即可
+            yield Request(url= parse.urljoin(response.url,post_url),callback=self.parse_detail)
 
+        '''2-提取下一页进行下载'''
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            # 此时回调函数继续执行是parse函数，即获取列表的所有文章地址，进行解析(整个流程是个递归，感觉）
+            yield Request(url = parse.urljoin(response.url,next_url),callback=self.parse)
 
+    '''提取文章的具体字段'''
     def parse_detail(self,response):
         # 得到标题
         # re_selector = response.xpath("/html/body/div[3]/div[3]/div[1]/div[1]/h1")
         # re2_selector = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()')
-        title1 = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract()[0]
-        create_date1 = response.xpath('//*[@id="post-110287"]/div[2]/p/text()').extract()[0].strip().replace("·","").strip()
+        # title1 = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract()[0]
+        # create_date1 = response.xpath('//*[@id="post-110287"]/div[2]/p/text()').extract()[0].strip().replace("·","").strip()
 
         title = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0]
         create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace('·','').strip()
@@ -42,13 +53,17 @@ class JobboleSpider(scrapy.Spider):
         # 注意这里要非贪婪匹配
         match_re = re.match(".*?(\d+).*",fav_nums);
         if match_re:
-            fav_nums = match_re.group(1)
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
 
         # 评论数
         comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
         match_re2 = re.match(".*?(\d+).*",comment_nums);
         if match_re2:
-            comment_nums = match_re2.group(1)
+            comment_nums = int(match_re2.group(1))
+        else:
+            comment_nums = 0
 
         # 内容
         content = response.xpath("//div[@class='entry']").extract()[0]
